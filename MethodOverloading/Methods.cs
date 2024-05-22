0a1fabda15fb109f93d8022e567459013e018b0a
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-05-22 13:58:12 -0400

    initial commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading;
+internal class Methods
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static decimal Add(decimal a, decimal b)
+    {
+        return a + b;
+    }
+
+    public static string Add(int a, int b, bool dollars)
+    {
+        var sum = a + b;
+
+        if (dollars)
+        {
+            if (sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return $"{sum} dollars";
+            }
+        }
+        else
+        {
+            return sum.ToString();
+        }
+    }
+}
+
+
+  
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,8 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿ using MethodOverloading;
+using System.Runtime.Intrinsics.X86;
+
+    var answer = Methods.Add(7, 8);
+    decimal answer2 = Methods.Add(9.2m, 10.1m);
+    string answer3 = Methods.Add(4, 8, true);
+    Console.WriteLine($"{answer}, {answer2}, {answer3}");
+
